d9c6785b212e1078d4ac868f66702290e26b5e03
GustavoPinheiro170 <gustavocampos170@hotmail.com>
2020-10-24 00:47:33 -0300

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import './index.css';
 import { UserStorage } from './UserContext';
-import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <UserStorage>
           <Header />
              <App />
           <Footer />
         </UserStorage>
-      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
